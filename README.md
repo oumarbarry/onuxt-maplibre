17a789c8b44cff5d5a015be6396e1ffdb6aff10f
Oumar Barry <oumar.barry1202@gmail.com>
2024-08-18 00:52:24 +0000

    chore: init

---
+++ README.md
@@ -0,0 +1,19 @@
+# onuxt-maplibre
+
+## Usage
+
+### GitHub Template
+
+[Create a repo from this template on GitHub](https://github.com/oumarbarry/onuxt-maplibre/generate)
+
+### Clone to local
+
+Use [giget](https://github.com/unjs/giget) to clone to your machine with an empty git history:
+
+```bash
+bunx giget@latest gh:oumarbarry/onuxt-maplibre nuxt-app --install --shell
+```
+
+## Contribution
+
+Feel free to contribute 💻
---
+++ app/app.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+// const style = "https://api.maptiler.com/maps/streets/style.json?key=cQX2iET1gmOW38bedbUh"
+const style = "https://demotiles.maplibre.org/style.json"
+</script>
+
+<template>
+  <div class="flex h-svh w-screen">
+    <div id="actual-page">
+      <MglMap
+        :map-style="style"
+        :center="[-13, 9]"
+        :zoom="3"
+      >
+        <MglFullscreenControl />
+        <MglNavigationControl />
+        <MglGeolocateControl />
+        <MglScaleControl />
+
+        <MglMarker :coordinates="[-13.6, 9.6004923]" color="#cc0000" draggable>
+          <mgl-popup>
+            <h1 class="text-black">
+              Hello
+            </h1>
+          </mgl-popup>
+        </MglMarker>
+      </MglMap>
+    </div>
+  </div>
+</template>
+
+<style>
+#actual-page {
+  width: 100%;
+}
+
+#actual-page > div {
+  height: 100%;
+}
+</style>
